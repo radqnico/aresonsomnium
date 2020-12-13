18e6fe509b62091b5b82e01ef62ec9db850a6eae
Nicolò Scialpi <nicoloscialpi@gmail.com>
2020-12-13 13:21:42 +0100

    Fix JPA

--- src/main/java/it/areson/aresonsomnium/AresonSomnium.java
+++ src/main/java/it/areson/aresonsomnium/AresonSomnium.java
@@ -2,10 +2,12 @@ package it.areson.aresonsomnium;
 
 import it.areson.aresonsomnium.entities.SomniumPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.Properties;
 
 public class AresonSomnium extends JavaPlugin {
 
@@ -20,8 +22,10 @@ public class AresonSomnium extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("aresonSomnium");
-        entityManager = entityManagerFactory.createEntityManager();
+
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+
+        entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit");
 
         SomniumPlayer somniumPlayer = new SomniumPlayer();
 
