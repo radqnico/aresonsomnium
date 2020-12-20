321b402e431c1ae28b645e7c8fcdbf7bf8a181e1
Nicolò Scialpi <nicoloscialpi@gmail.com>
2020-12-20 22:18:19 +0100

    Fix database name in MySqlConfig

--- src/main/java/it/areson/aresonsomnium/database/MySqlConfig.java
+++ src/main/java/it/areson/aresonsomnium/database/MySqlConfig.java
@@ -5,7 +5,7 @@ public class MySqlConfig {
     public static String HOST = "localhost";
     public static String USER = "root";
     public static String PASS = "CKK6z9P3hKyCgvk4";
-    public static String DB = "areson_minigames_stats";
+    public static String DB = "aresonSomnium";
 
     public static String PLAYER_TABLE_NAME = "somniumPlayer";
 
