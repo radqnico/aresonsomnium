69332d8ae74bb52ab67e3c1e095c988a5e280fcf
Nicolò Scialpi <nicoloscialpi@gmail.com>
2020-12-23 18:06:41 +0100

    Test clicked inventory clone object

--- src/main/java/it/areson/aresonsomnium/listeners/CustomGuiEventsListener.java
+++ src/main/java/it/areson/aresonsomnium/listeners/CustomGuiEventsListener.java
@@ -3,12 +3,16 @@ package it.areson.aresonsomnium.listeners;
 import it.areson.aresonsomnium.AresonSomnium;
 import it.areson.aresonsomnium.shops.GuiManager;
 import it.areson.aresonsomnium.utils.MessageUtils;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class CustomGuiEventsListener extends GeneralEventListener {
@@ -41,7 +45,17 @@ public class CustomGuiEventsListener extends GeneralEventListener {
         if (guiManager.isViewingCustomGui(player)) {
             Inventory clickedInventory = event.getClickedInventory();
             if(Objects.nonNull(clickedInventory)) {
-                System.out.println(clickedInventory.getType());
+                if(clickedInventory.getType().equals(InventoryType.CHEST)){
+                    ItemStack currentItem = event.getCurrentItem();
+                    if(Objects.nonNull(currentItem) && !currentItem.getType().equals(Material.AIR)){
+                        HashMap<Integer, ItemStack> integerItemStackHashMap = player.getInventory().addItem(currentItem.clone());
+                        if(integerItemStackHashMap.isEmpty()){
+                            // Scala monete
+                        } else {
+                            player.sendMessage(MessageUtils.errorMessage("Hai l'inventario pieno!"));
+                        }
+                    }
+                }
             }
             event.setCancelled(true);
         }
