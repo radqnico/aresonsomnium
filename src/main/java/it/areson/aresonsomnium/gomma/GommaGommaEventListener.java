95498fdc72486f8636a648894cb022b5f4b9f0f8
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-01-18 15:24:36 +0100

    Fixed event called twice

--- src/main/java/it/areson/aresonsomnium/gomma/GommaGommaEventListener.java
+++ src/main/java/it/areson/aresonsomnium/gomma/GommaGommaEventListener.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -21,13 +22,15 @@ public class GommaGommaEventListener extends GeneralEventListener {
 
     @EventHandler
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
-        Block clickedBlock = event.getClickedBlock();
-        if (Objects.nonNull(clickedBlock)) {
-            Location location = clickedBlock.getLocation();
-            Location gommaBlockLocation = aresonSomnium.getGommaObjectsFileReader().getGommaBlock();
-            if (location.equals(gommaBlockLocation)) {
-                gommaPreconditionsOk(event.getPlayer());
-                event.setCancelled(true);
+        if (EquipmentSlot.HAND.equals(event.getHand())) {
+            Block clickedBlock = event.getClickedBlock();
+            if (Objects.nonNull(clickedBlock)) {
+                Location location = clickedBlock.getLocation();
+                Location gommaBlockLocation = aresonSomnium.getGommaObjectsFileReader().getGommaBlock();
+                if (location.equals(gommaBlockLocation)) {
+                    gommaPreconditionsOk(event.getPlayer());
+                    event.setCancelled(true);
+                }
             }
         }
     }
