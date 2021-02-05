518e91945b233528c9f7e526e65b0857fff1062f
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-02-05 15:50:46 +0100

    WIP assegni (test apply)

--- src/main/java/it/areson/aresonsomnium/listeners/CheckApplyListener.java
+++ src/main/java/it/areson/aresonsomnium/listeners/CheckApplyListener.java
@@ -3,7 +3,6 @@ package it.areson.aresonsomnium.listeners;
 import it.areson.aresonsomnium.AresonSomnium;
 import it.areson.aresonsomnium.economy.Wallet;
 import it.areson.aresonsomnium.players.SomniumPlayer;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -19,13 +18,15 @@ public class CheckApplyListener extends GeneralEventListener {
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
         if (EquipmentSlot.HAND.equals(event.getHand())) {
-            Bukkit.getLogger().warning("CIAONE");
             ItemStack item = event.getItem();
             if (item != null && !Material.AIR.equals(item.getType())) {
                 if (Material.PAPER.equals(item.getType())) {
                     SomniumPlayer somniumPlayer = aresonSomnium.getSomniumPlayerManager().getSomniumPlayer(event.getPlayer());
                     if (somniumPlayer != null) {
-                        Wallet.applyCheck(somniumPlayer, item);
+                        if (Wallet.applyCheck(somniumPlayer, item)) {
+                            item.setAmount(item.getAmount() - 1);
+                            event.getPlayer().sendMessage(aresonSomnium.getMessageManager().getPlainMessage("check-applied"));
+                        }
                     } else {
                         aresonSomnium.getDebugger().debugError(aresonSomnium.getMessageManager().getPlainMessage("somniumplayer-not-found"));
                     }
