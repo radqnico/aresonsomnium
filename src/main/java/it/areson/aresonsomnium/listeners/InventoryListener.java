92a1d0ea8b87b90d4165556a9486a9d93bd6fccc
Marco Paggioro <marco.paggioro@gmail.com>
2021-02-18 18:36:46 +0100

    WIP Click enchants

---
+++ src/main/java/it/areson/aresonsomnium/listeners/InventoryListener.java
@@ -0,0 +1,24 @@
+package it.areson.aresonsomnium.listeners;
+
+import it.areson.aresonsomnium.AresonSomnium;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryListener extends GeneralEventListener {
+
+    public InventoryListener(AresonSomnium aresonSomnium) {
+        super(aresonSomnium);
+    }
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent event) {
+        HumanEntity whoClicked = event.getWhoClicked();
+        if(whoClicked instanceof Player) {
+            Player player = (Player) whoClicked;
+            player.sendMessage(event.getCursor().toString());
+            player.sendMessage(event.getCurrentItem().toString());
+        }
+    }
+}
