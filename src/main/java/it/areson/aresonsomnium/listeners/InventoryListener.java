7fdc464846a93e29e464d7a8150e75bce70a7cb3
Marco Paggioro <marco.paggioro@gmail.com>
2021-02-18 19:05:32 +0100

    WIP Click enchants

--- src/main/java/it/areson/aresonsomnium/listeners/InventoryListener.java
+++ src/main/java/it/areson/aresonsomnium/listeners/InventoryListener.java
@@ -1,10 +1,13 @@
 package it.areson.aresonsomnium.listeners;
 
 import it.areson.aresonsomnium.AresonSomnium;
+import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryListener extends GeneralEventListener {
 
@@ -15,10 +18,15 @@ public class InventoryListener extends GeneralEventListener {
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event) {
         HumanEntity whoClicked = event.getWhoClicked();
-        if(whoClicked instanceof Player) {
+        if(whoClicked instanceof Player && event.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)) {
             Player player = (Player) whoClicked;
-            player.sendMessage(event.getCursor().toString());
-            player.sendMessage(event.getCurrentItem().toString());
+            ItemStack handItemStack = event.getCursor();
+            ItemStack clickedItemStack = event.getCurrentItem();
+
+            if(handItemStack != null && handItemStack.getType().equals(Material.ENCHANTED_BOOK) && clickedItemStack != null) {
+
+                player.sendMessage("Okay");
+            }
         }
     }
 
