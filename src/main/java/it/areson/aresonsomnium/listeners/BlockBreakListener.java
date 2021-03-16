49b3096572029bd31ddf34f2e18ed70a11dd9007
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-03-16 16:33:00 +0100

    Added block break event

--- src/main/java/it/areson/aresonsomnium/listeners/BlockBreakListener.java
+++ src/main/java/it/areson/aresonsomnium/listeners/BlockBreakListener.java
@@ -1,6 +1,7 @@
 package it.areson.aresonsomnium.listeners;
 
 import it.areson.aresonsomnium.AresonSomnium;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -19,11 +20,17 @@ public class BlockBreakListener extends GeneralEventListener {
     public void onBlockBreak(BlockBreakEvent e) {
         if (!e.isCancelled()) {
             Collection<ItemStack> drops = e.getBlock().getDrops();
+            boolean shouldDestroy = false;
             for (ItemStack itemStack : drops) {
                 if (!e.getPlayer().getInventory().addItem(itemStack).isEmpty()) {
                     e.setCancelled(true);
                     return;
+                } else {
+                    shouldDestroy = true;
                 }
+            }
+            if (shouldDestroy) {
+                e.getBlock().setType(Material.AIR);
             }
         }
     }
