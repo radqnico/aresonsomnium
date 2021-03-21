5ee84fe0bfb4f7f91a0d28a170cebe335c4d6b1e
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-03-21 12:04:44 +0100

    Debug checks

--- src/main/java/it/areson/aresonsomnium/economy/Wallet.java
+++ src/main/java/it/areson/aresonsomnium/economy/Wallet.java
@@ -87,7 +87,7 @@ public class Wallet {
                     String secondLine = lore.get(1);
                     String clean = secondLine.replaceAll("&.", "").replaceAll("§.", "");
                     Pattern patternAmount = Pattern.compile("[0-9.]+([E][0-9]+)?");
-                    Pattern patternCoinType = Pattern.compile("[a-zA-Z]+");
+                    Pattern patternCoinType = Pattern.compile("(monete|gemme|oboli)");
                     Matcher matcherCoinType = patternCoinType.matcher(clean);
                     Matcher matcherAmount = patternAmount.matcher(clean);
                     if (matcherAmount.find() && matcherCoinType.find()) {
