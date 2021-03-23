6624065924441723356911340b92d5c4695176fd
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-03-23 22:08:38 +0100

    Debug inventory items

--- src/main/java/it/areson/aresonsomnium/economy/shops/listener/CustomGuiEventsListener.java
+++ src/main/java/it/areson/aresonsomnium/economy/shops/listener/CustomGuiEventsListener.java
@@ -74,7 +74,7 @@ public class CustomGuiEventsListener extends GeneralEventListener {
     public void onInventoryClickEvent(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
         Inventory clickedInventory = event.getClickedInventory();
-        System.out.println("Event: " + event.getAction().name());
+        System.out.println("Event: " + event.getClickedInventory().getItem(0).getType().name());
         if (clickedInventory != null) {
             if (shopEditor.isEditingCustomGui(player)) {
                 // Editing
