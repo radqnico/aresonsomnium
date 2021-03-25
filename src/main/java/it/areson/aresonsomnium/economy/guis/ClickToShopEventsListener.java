132aad37f032cc394743faecd3c5f4c4aeac5f2e
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-03-25 18:50:12 +0100

    Debug click to shop

--- src/main/java/it/areson/aresonsomnium/economy/guis/ClickToShopEventsListener.java
+++ src/main/java/it/areson/aresonsomnium/economy/guis/ClickToShopEventsListener.java
@@ -7,10 +7,7 @@ import it.areson.aresonsomnium.economy.items.ShopItem;
 import it.areson.aresonsomnium.listeners.GeneralEventListener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -28,22 +25,17 @@ public class ClickToShopEventsListener extends GeneralEventListener {
         Inventory clickedInventory = event.getClickedInventory();
         Inventory topInventory = event.getView().getTopInventory();
         Player player = (Player) event.getWhoClicked();
-        System.out.println(AresonSomniumAPI.instance.commandPanelsAPI.isPanelOpen(player));
         if (Objects.equals(clickedInventory, topInventory)) {
-            System.out.println("Objects.equals(clickedInventory, topInventory)");
             // CLick su inventario in alto
             if (isLeftClicking(event)) {
-                System.out.println("isLeftClicking(event)");
-                System.out.println(AresonSomniumAPI.instance.commandPanelsAPI.isPanelOpen(player));
                 // Click sinistro pulito
                 ItemStack item = clickedInventory.getItem(event.getSlot());
                 if (item != null) {
-                    System.out.println("item != null");
+                    System.out.println("item != null  " + item.getType());
                     // Cerco ID del mio plugin
                     int idFromItemData = ShopItem.getIdFromItem(item);
                     if (idFromItemData != -1) {
                         System.out.println("idFromItemData != -1");
-                        System.out.println(AresonSomniumAPI.instance.commandPanelsAPI.isPanelOpen(player));
                         // Se c'è ID su oggetto, vedo se lo conosco
                         if (AresonSomniumAPI.instance.commandPanelsAPI.isPanelOpen(player)) {
                             System.out.println("AresonSomniumAPI.instance.commandPanelsAPI.isPanelOpen(player)");
