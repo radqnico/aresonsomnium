8de37c53cee97babb74bfa567948f7d0125fd490
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-03-30 23:31:36 +0200

    Moved code in function

--- src/main/java/it/areson/aresonsomnium/commands/repair/SomniumRepairCommand.java
+++ src/main/java/it/areson/aresonsomnium/commands/repair/SomniumRepairCommand.java
@@ -1,13 +1,17 @@
 package it.areson.aresonsomnium.commands.repair;
 
 import it.areson.aresonsomnium.AresonSomnium;
+import it.areson.aresonsomnium.economy.Price;
 import it.areson.aresonsomnium.elements.Pair;
+import it.areson.aresonsomnium.players.SomniumPlayer;
 import org.bukkit.Material;
 import org.bukkit.command.*;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -39,33 +43,53 @@ public class SomniumRepairCommand implements CommandExecutor, TabCompleter {
                 Player player = aresonSomnium.getServer().getPlayer(playerName);
                 if (player != null) {
                     ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
-                    if (!Objects.equals(itemInMainHand.getType(), Material.AIR)) {
-                        Pair<Boolean, String> booleanStringPair = repairCountdown.canRepair(playerName);
-                        if (booleanStringPair.left()) {
-                            ItemMeta itemMeta = itemInMainHand.getItemMeta();
-                            if (itemMeta instanceof Damageable) {
-                                Damageable damageable = (Damageable) itemMeta;
-                                if (damageable.getDamage() != 0) {
-                                    repairCountdown.setLastRepairTime(playerName);
-                                    damageable.setDamage(0);
-                                    itemInMainHand.setItemMeta((ItemMeta) damageable);
-                                    player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repaired-success"));
-                                }else{
-                                    player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repaired-no-damage"));
-                                }
-                            } else {
-                                player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repaired-cant-repair"));
-                            }
-                        } else {
-                            player.sendMessage(booleanStringPair.right());
-                        }
-                    }
+                    repairSingleItem(player, itemInMainHand);
                 }
             } else {
                 commandSender.sendMessage("Scrivi il nome del giocatore");
             }
         }
         return true;
+    }
+
+    private void repairSingleItem(Player player, ItemStack itemStack) {
+        SomniumPlayer somniumPlayer = aresonSomnium.getSomniumPlayerManager().getSomniumPlayer(player);
+        if (somniumPlayer != null) {
+            if (!Objects.equals(itemStack.getType(), Material.AIR)) {
+                Pair<Boolean, String> booleanStringPair = repairCountdown.canRepair(player.getName());
+                if (booleanStringPair.left()) {
+                    ItemMeta itemMeta = itemStack.getItemMeta();
+                    if (itemMeta instanceof Damageable) {
+                        Damageable damageable = (Damageable) itemMeta;
+                        if (damageable.getDamage() != 0) {
+                            repairCountdown.setLastRepairTime(player.getName());
+                            damageable.setDamage(0);
+                            itemStack.setItemMeta((ItemMeta) damageable);
+                            Price repairPrice = getRepairPriceFromConfig(aresonSomnium);
+                            if (somniumPlayer.takePriceAmount(repairPrice)) {
+                                player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repair-success"));
+                            } else {
+                                player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repair-not-enough-coins"));
+                            }
+                        } else {
+                            player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repaired-no-damage"));
+                        }
+                    } else {
+                        player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repaired-cant-repair"));
+                    }
+                } else {
+                    player.sendMessage(booleanStringPair.right());
+                }
+            }
+        }
+    }
+
+    private Price getRepairPriceFromConfig(JavaPlugin plugin) {
+        FileConfiguration config = plugin.getConfig();
+        int costCoins = config.getInt("repair.cost.coins");
+        int costObols = config.getInt("repair.cost.obols");
+        int costGems = config.getInt("repair.cost.gems");
+        return new Price(costCoins, costObols, costGems);
     }
 
     @Override
