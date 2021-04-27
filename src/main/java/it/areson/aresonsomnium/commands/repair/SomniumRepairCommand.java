cd38252ff9eb21531e8844006a10d9f57de1d945
Nicolò Scialpi <nicoloscialpi@gmail.com>
2021-04-27 20:55:09 +0200

    Repair with gems test 1

--- src/main/java/it/areson/aresonsomnium/commands/repair/SomniumRepairCommand.java
+++ src/main/java/it/areson/aresonsomnium/commands/repair/SomniumRepairCommand.java
@@ -1,6 +1,7 @@
 package it.areson.aresonsomnium.commands.repair;
 
 import it.areson.aresonsomnium.AresonSomnium;
+import it.areson.aresonsomnium.economy.CoinType;
 import it.areson.aresonsomnium.economy.Price;
 import it.areson.aresonsomnium.elements.Pair;
 import it.areson.aresonsomnium.players.SomniumPlayer;
@@ -40,17 +41,30 @@ public class SomniumRepairCommand implements CommandExecutor, TabCompleter {
         if (commandSender.hasPermission("aresonsomnium.admin")) {
             if (arguments.length == 2) {
                 String playerName = arguments[0];
-                String coinType = arguments[1];
+                CoinType type = CoinType.valueOf(arguments[1].toUpperCase());
                 Player player = aresonSomnium.getServer().getPlayer(playerName);
                 if (player != null) {
-                    ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
-                    repairSingleItemCoins(player, itemInMainHand);
+                    switchActionCoins(player, type);
                 }
             } else {
                 commandSender.sendMessage("Scrivi il nome del giocatore");
             }
         }
         return true;
+    }
+
+    public void switchActionCoins(Player player, CoinType coinType) {
+        ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
+        switch (coinType) {
+            case MONETE:
+                repairSingleItemCoins(player, itemInMainHand);
+                break;
+            case GEMME:
+                repairSingleItemGems(player, itemInMainHand);
+                break;
+            case OBOLI:
+                break;
+        }
     }
 
     private void repairSingleItemCoins(Player player, ItemStack itemStack) {
@@ -66,7 +80,7 @@ public class SomniumRepairCommand implements CommandExecutor, TabCompleter {
                             repairCountdown.setLastRepairTime(player.getName());
                             damageable.setDamage(0);
                             itemStack.setItemMeta((ItemMeta) damageable);
-                            Price repairPrice = getRepairPriceFromConfig(aresonSomnium);
+                            Price repairPrice = getRepairPriceFromConfig(aresonSomnium, CoinType.MONETE);
                             if (somniumPlayer.takePriceAmount(repairPrice)) {
                                 player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repair-success"));
                             } else {
@@ -85,12 +99,47 @@ public class SomniumRepairCommand implements CommandExecutor, TabCompleter {
         }
     }
 
-    private Price getRepairPriceFromConfig(JavaPlugin plugin) {
+    private void repairSingleItemGems(Player player, ItemStack itemStack) {
+        SomniumPlayer somniumPlayer = aresonSomnium.getSomniumPlayerManager().getSomniumPlayer(player);
+        if (somniumPlayer != null) {
+            if (!Objects.equals(itemStack.getType(), Material.AIR)) {
+                ItemMeta itemMeta = itemStack.getItemMeta();
+                if (itemMeta instanceof Damageable) {
+                    Damageable damageable = (Damageable) itemMeta;
+                    if (damageable.getDamage() != 0) {
+                        repairCountdown.setLastRepairTime(player.getName());
+                        damageable.setDamage(0);
+                        itemStack.setItemMeta((ItemMeta) damageable);
+                        Price repairPrice = getRepairPriceFromConfig(aresonSomnium, CoinType.GEMME);
+                        if (somniumPlayer.takePriceAmount(repairPrice)) {
+                            player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repair-success"));
+                        } else {
+                            player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repair-not-enough-coins"));
+                        }
+                    } else {
+                        player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repair-no-damage"));
+                    }
+                } else {
+                    player.sendMessage(aresonSomnium.getMessageManager().getPlainMessage("repair-cant-repair"));
+                }
+            }
+        }
+    }
+
+    private Price getRepairPriceFromConfig(JavaPlugin plugin, CoinType coinType) {
         FileConfiguration config = plugin.getConfig();
         int costCoins = config.getInt("repair.cost.coins");
         int costObols = config.getInt("repair.cost.obols");
         int costGems = config.getInt("repair.cost.gems");
-        return new Price(costCoins, costObols, costGems);
+        switch (coinType){
+            case MONETE:
+                return new Price(costCoins, 0, 0);
+            case GEMME:
+                return new Price(0, 0, costGems);
+            case OBOLI:
+                return new Price(0, costObols, 0);
+        }
+        throw new IllegalArgumentException("Cointype is not valid");
     }
 
     @Override
