288caa6542fcf9a25155bc998e902b4e720e66a6
Marco Paggioro <marco.paggioro@gmail.com>
2021-10-27 23:28:52 +0200

    CQI

--- src/main/java/it/areson/aresonsomnium/books/BookBuilder.java
+++ src/main/java/it/areson/aresonsomnium/books/BookBuilder.java
@@ -1,20 +1,16 @@
 package it.areson.aresonsomnium.books;
 
-import com.google.common.base.Splitter;
 import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class BookBuilder {
-    private ItemStack writtenBook;
-    private BookMeta bookMeta;
-    private final int charactersPerPage = 256;
+    private final ItemStack writtenBook;
+    private final BookMeta bookMeta;
 
     public BookBuilder() {
         this.writtenBook = new ItemStack(Material.WRITTEN_BOOK);
@@ -24,7 +20,7 @@ public class BookBuilder {
     public void buildWrittenBook(String title, String author, Iterable<String> pages) {
         this.bookMeta.setTitle(title);
         this.bookMeta.setAuthor(author);
-        for (String page: pages) {
+        for (String page : pages) {
             String colouredText = ChatColor.translateAlternateColorCodes('&', page);
             this.bookMeta.addPages(Component.text(colouredText));
         }
@@ -34,17 +30,17 @@ public class BookBuilder {
     public void buildWrittenBook(String title, String author, String content) {
         LinkedList<String> sortedPages = new LinkedList<>();
         String[] splitContent = content.split(" ");
-        int lastPage = 0;
-        String page = "";
-        for(String word: splitContent) {
-            if(page.length() + word.length() < this.charactersPerPage) {
-                page += word + " ";
+        StringBuilder page = new StringBuilder();
+        for (String word : splitContent) {
+            int charactersPerPage = 256;
+            if (page.length() + word.length() < charactersPerPage) {
+                page.append(word).append(" ");
             } else {
-                sortedPages.add(page);
-                page = word + " ";
+                sortedPages.add(page.toString());
+                page = new StringBuilder(word + " ");
             }
         }
-        sortedPages.add(page);
+        sortedPages.add(page.toString());
         this.buildWrittenBook(title, author, sortedPages);
     }
 
