22375cd5f1be80268b13a7e593d6bcf01d25c3bb
Marco Paggioro <marco.paggioro@gmail.com>
2021-12-17 13:33:07 +0100

    Steal on death with world filter

--- src/main/java/it/areson/aresonsomnium/listeners/PlayerListener.java
+++ src/main/java/it/areson/aresonsomnium/listeners/PlayerListener.java
@@ -10,19 +10,26 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Optional;
 
 public class PlayerListener extends GeneralEventListener {
 
+    private final HashSet<String> stealCoinsWorlds;
+
     public PlayerListener(AresonSomnium aresonSomnium) {
         super(aresonSomnium);
         registerEvents();
+
+        stealCoinsWorlds = new HashSet<>(aresonSomnium.getConfig().getStringList("steal-coins-worlds"));
     }
 
     @EventHandler
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
         // Listen for last hit
-        if (event.getDamager() instanceof Player damager && event.getEntity() instanceof Player damaged) {
+        if (event.getDamager() instanceof Player damager &&
+                event.getEntity() instanceof Player damaged &&
+                stealCoinsWorlds.contains(damaged.getWorld().getName())) {
             AresonSomniumAPI.instance.getLastHitPvP().setLastHit(damager, damaged);
         }
     }
@@ -30,7 +37,9 @@ public class PlayerListener extends GeneralEventListener {
     @EventHandler
     public void onEntityDamageEvent(EntityDamageEvent event) {
         // Listen for death
-        if (event.getEntity() instanceof Player damaged && event.getDamage() >= damaged.getHealth()) {
+        if (event.getEntity() instanceof Player damaged &&
+                event.getDamage() >= damaged.getHealth() &&
+                stealCoinsWorlds.contains(damaged.getWorld().getName())) {
             Optional<Player> killer = AresonSomniumAPI.instance.getLastHitPvP().getKiller(damaged);
             killer.ifPresent(playerKiller -> {
                 SomniumPlayer somniumPlayerKiller = AresonSomniumAPI.instance.getSomniumPlayerManager().getSomniumPlayer(playerKiller);
