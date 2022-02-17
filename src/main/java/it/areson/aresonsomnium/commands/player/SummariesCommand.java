ea3e6b3975d5787b0d38f12aca12886c8a0025bc
Marco Paggioro <marco.paggioro@gmail.com>
2022-02-18 00:04:17 +0100

    WIP Summaries

--- src/main/java/it/areson/aresonsomnium/commands/player/SummariesCommand.java
+++ src/main/java/it/areson/aresonsomnium/commands/player/SummariesCommand.java
@@ -2,21 +2,29 @@ package it.areson.aresonsomnium.commands.player;
 
 import it.areson.aresonlib.commands.shapes.RegisteredCommand;
 import it.areson.aresonsomnium.AresonSomnium;
-import it.areson.aresonsomnium.books.BookBuilder;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+
+import java.util.HashMap;
+
+import static org.bukkit.ChatColor.*;
+
 
 @SuppressWarnings("NullableProblems")
 public class SummariesCommand extends RegisteredCommand {
 
     private final AresonSomnium aresonSomnium;
+    private HashMap<Integer, ItemStack> books;
 
     public SummariesCommand(AresonSomnium aresonSomnium, String command) {
         super(aresonSomnium, command);
         this.aresonSomnium = aresonSomnium;
-        initialize();
+        initializeBooks();
     }
 
     //TODO
@@ -26,20 +34,7 @@ public class SummariesCommand extends RegisteredCommand {
             if (arguments.length == 1) {
                 try {
                     int briefNumber = Integer.parseInt(arguments[0]);
-                    BookBuilder builder = new BookBuilder();
-                    YamlConfiguration yamlConfiguration = aresonSomnium.getSummariesFileManager().getYamlConfiguration();
-                    String path = String.format("riassunti.%d", briefNumber);
-                    if (yamlConfiguration.isConfigurationSection(path)) {
-                        String chapterTitle = yamlConfiguration.getString(path + ".titolo");
-                        String title = chapterTitle.length() > 32 ? "Blank" : chapterTitle;
-                        String author = "Areson";
-                        String content = "&c&l" + chapterTitle + "&r&0\n\n" + yamlConfiguration.getString(path + ".testo");
-                        builder.buildWrittenBook(title, author, content);
-                        player.openBook(builder.getWrittenBook());
-                    } else {
-                        String message = String.format("Il riassunto numero %d non esiste. Inserisci un numero di riassunto valido.", briefNumber);
-                        player.sendMessage(message);
-                    }
+                    //player.openBook(builder.getWrittenBook());
                 } catch (NumberFormatException exception) {
                     player.sendMessage("Errore. Devi inserire il numero di riassunto. '" + arguments[0] + "' non è un numero.");
                 }
@@ -48,8 +43,26 @@ public class SummariesCommand extends RegisteredCommand {
         return true;
     }
 
-    public void initialize() {
+    public void initializeBooks() {
+        books = new HashMap<>();
+        ConfigurationSection summaries = aresonSomnium.getSummariesFileManager().getYamlConfiguration().getConfigurationSection("summaries");
+        if (summaries != null) {
+            for (String key : summaries.getKeys(false)) {
+                String title = summaries.getString(key + ".title");
+                String description = summaries.getString(key + ".description");
+                String totalContent = RED + "" + BOLD + title + "\n\n" + BLACK + description;
+                books.put(key, buildBook(totalContent));
+            }
+        }
+    }
 
+    public ItemStack buildBook(String content) {
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        BookMeta bookMeta = (BookMeta) book.getItemMeta();
+
+
+        book.setItemMeta(bookMeta);
+        return book;
     }
 
 }
